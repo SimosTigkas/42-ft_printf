285f1da996e31d4a6b97a9d590c09e3cc93cc1e8
Simos Tigkas <stigkas@c1r6p18.hive.fi>
2023-11-16 12:44:08 +0200

    added ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 08:32:06 by stigkas           #+#    #+#             */
-/*   Updated: 2023/11/16 12:27:07 by stigkas          ###   ########.fr       */
+/*   Updated: 2023/11/16 12:42:52 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,30 +36,4 @@ int	ft_printf(const char *format, ...)
 	}
 	va_end(lst_args);
 	return (char_print);
-}
-
-int	main(void)
-{
-	int	count;
-	long int num = 10;
-    void *ptr;
-
-	ptr = &num;
-	count = ft_printf("%u\n", INT_MAX + 1);
-	ft_printf("The chars written are %i\n", count);
-	count = printf("%u\n", INT_MAX + 1);
-	printf("The chars written are %i\n", count);
-	count = ft_printf("%d\n", 052);
-	ft_printf("The chars written are %d\n", count);
-	count = printf("%d\n", 052);
-	printf("The chars written are %d\n", count);
-	ft_printf("Hello %s, today is %i, in hex %X, char -> %c\n",
-				"Simos",
-				 15,
-				  15,
-				   's');
-	ft_printf("%%\n");
-    printf("Address of num: %p\n", ptr);
-    ft_printf("Address of num: %p\n", ptr);
-	return (0);
 }
